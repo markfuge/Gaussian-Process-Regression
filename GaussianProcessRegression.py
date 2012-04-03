57a9a0f216ef65c4fc445503f38c332f26e3a5af
Mark Fuge <markfuge@gmail.com>
2012-04-03 02:46:53 -0700

    Finished data partioning, testing, and output. Ready to merge back to master branch.

--- GaussianProcessRegression.py
+++ GaussianProcessRegression.py
@@ -34,5 +34,32 @@ x = cooked[1]
 #graph_abalone_data(y,x,"Cooked",1)
 #graph_abalone_data(raw[0],raw[1],"Raw",2)
 
-gpr = GPRegressor()
-gpr.approximate_quantiles(x)
+num_partions = 5
+# Initialize the regressor
+gpr = GPRegressor(x,y,num_partions)
+
+# Test Values
+sigma = [0.01, 0.03, 0.1, 0.3, 1., 3.]
+Lambda = gpr.return_quantiles()
+results=np.empty((len(sigma),len(Lambda),2))
+
+# Run Tests
+output = open('results.csv','w')
+
+# Keep the same lambda across different sigmas, to save kernal computation
+for v,l in enumerate(Lambda):
+    gpr.reset_lambda(l)
+    for u,s in enumerate(sigma):
+        avg_v_error = 0
+        avg_t_error = 0
+        for i in range(0,num_partions):
+            v_err,t_err = gpr.estimate_mean(i,s,l)
+            avg_v_error += v_err
+            avg_t_error += t_err
+        avg_v_error /= num_partions
+        avg_t_error /= num_partions
+        outstring = 's: {}, l: {}, verr: {}, terr: {}\n'.format(s,l,avg_v_error,avg_t_error)
+        print outstring
+        output.write(outstring)
+
+output.close()
--- gpregressor.py
+++ gpregressor.py
@@ -2,6 +2,7 @@ import random as random
 import numpy as np
 import math
 import scipy.linalg
+import itertools
 
 class Kernal(object):
     matrix= np.matrix(float)
@@ -25,6 +26,24 @@ class Kernal(object):
         sq_norms=d.transpose()*(ones_d) + ones_d2.transpose()*d2 - 2*X*X2.transpose()
         return np.exp(-sq_norms/(self.Lambda**2))   # Using Gaussian RBF Kernal
 
+    def get_kernal_partions(self,partion_number,partion_indices,remainder=False):
+        """Returns a set of partions of the kernal matrix.
+           Given a partion of indices P*, this function returns 4 matrices:
+           K(P*,P*),K(P*,P),K(P,P*),K(P,P)
+           where P represents the remaining partions
+        """
+        #partion_indices = self.partion_indices  # copy the partions
+        pi = list(partion_indices)
+        target_inds = pi[partion_number]
+        pi.pop(partion_number)
+        # Now add the rest of the indices together
+        remain_inds = list(itertools.chain(*pi))
+        ix1 = np.ix_(target_inds,target_inds)
+        ix2 = np.ix_(target_inds,remain_inds)
+        ix3 = np.ix_(remain_inds,target_inds)
+        ix4 = np.ix_(remain_inds,remain_inds)
+        K = self.matrix
+        return (K[ix1],K[ix2],K[ix3],K[ix4])
 
     def store_kernal_matrix(self,x,x2):
         self.matrix = self.compute_kernal_matrix(x,x2)
@@ -40,9 +59,33 @@ class Kernal(object):
 class GPRegressor(object):
     """Performs Gaussian Process Regression using a given Kernal"""
     quantiles = []
-    sigma = [0.01, 0.03, 0.1, 0.3, 1., 3.]
+    X=[]
+    y=[]
     kernal = Kernal()
+    partion_indices = []
     
+    def __init__(self,X,y,num_partions=5):
+        self.X=X
+        self.y=y
+        self.num_partions=num_partions
+        self.approximate_quantiles(X)
+        #self.init_kernal(X,y)
+        self.partion_data_indices(self.num_partions)
+
+    def reset_lambda(self,Lambda):
+        self.kernal.Lambda = Lambda
+        self.init_kernal(self.X,self.y)
+
+    def partion_data_indices(self,num_partions=None):
+        if num_partions is None:
+            num_partions = self.num_partions
+        N = int(self.X.shape[0])   # total number of rows
+        n = N/num_partions
+        indices = range(0,N)
+        for i in range(0,num_partions-1):
+            self.partion_indices.append(indices[n*i:n*(i+1)])
+        self.partion_indices.append(indices[n*(i+1):])
+
     def approximate_quantiles(self,x, num_samples=1000):
         """Randomly samples 1000 x pairs in order to compute various data quantiles.
            This is used for calibrating the kernal."""
@@ -54,14 +97,45 @@ class GPRegressor(object):
         norms.sort()
         quant_index = [ int(math.floor(len(norms)*quant))-1 for quant in [0.05,0.1,0.5,0.9,0.95]]
         self.quantiles = [ norms[ind] for ind in quant_index]
+    
+    def return_quantiles(self):
+        return self.quantiles
 
-    def estimate_mean(X,y,Xtest,sigma,Lambda):
-        I = np.eye(len(X))
-        K = self.kernal.get_kernal_matrix()
-        L = scipy.linalg.cho_factor(K + (sigma**2)*I)
+    def init_kernal(self,X,y):
+        self.kernal.store_kernal_matrix(X,X)
+
+    def get_y_partions(self,partion_number,partion_indices):
+        pi = list(partion_indices)
+        target_inds = pi[partion_number]
+        pi.pop(partion_number)
+        # Now add the rest of the indices together
+        remain_inds = list(itertools.chain(*pi))
+        return (self.y[target_inds],self.y[remain_inds])
+
+    def estimate_mean(self,partion_number,sigma,Lambda):
+        #K = self.kernal.get_kernal_matrix()
+        # Get the desired partions of the kernal matrix
+        KTKT,KTK,KKT,KK = self.kernal.get_kernal_partions(partion_number,self.partion_indices)
+        yT,y = self.get_y_partions(partion_number,self.partion_indices)
+        # Generate the additional noise diagonal matrix
+        I = np.eye(KK.shape[0])
+        # Factorize the non-test kernal matrix, with the added noise term
+        L = scipy.linalg.cho_factor(KK + (sigma**2)*I)
         LT = (L[0].transpose(),not L[1])
         a = scipy.linalg.cho_solve(L,y)
         alpha = scipy.linalg.cho_solve(LT,y)
+
+        # Now to evaluate the estimates
+        fT = KTK*alpha  # Estimated means on the validation data
+        validation_error = 0.5*np.square(yT-fT)
+        v_error_per = np.sum(validation_error)/int(yT.shape[0]) # get the error, per example
+
+        f = KK*alpha  # Estimated means on the training data
+        training_error = 0.5*np.square(y-f)
+        t_error_per = np.sum(training_error)/int(y.shape[0])
+
+        return v_error_per,t_error_per
+
         
 
         
